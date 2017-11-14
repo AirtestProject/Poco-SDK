068a71d0781ccbcabc22f41599b8c8e7865cf304
lxn3032 <lxn3032@corp.netease.com>
2017-11-14 11:06:46 +0800

    can dump invisible node at dumping hierarchy

---
+++ sdk/csharp/sdk/AbstractNode.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Poco
+{
+	public class AbstractNode
+	{
+		private List<string> requiredAttrs = new List<string> {
+			"name",
+			"type",
+			"visible",
+			"pos",
+			"size",
+			"scale",
+			"anchorPoint",
+			"zOrders"
+		};
+
+		public virtual AbstractNode getParent ()
+		{
+			return null;
+		}
+
+		public virtual List<AbstractNode> getChildren ()
+		{
+			return null;
+		}
+
+		public virtual object getAttr (string attrName)
+		{
+			Dictionary<string, object> defaultAttrs = new Dictionary<string, object> () {
+				{ "name", "<Root>" },
+				{ "type", "Root" },
+				{ "visible", true },
+				{ "pos", new List<float> (){ 0.0f, 0.0f } },
+				{ "size", new List<float> (){ 0.0f, 0.0f } },
+				{ "scale", new List<float> (){ 1.0f, 1.0f } },
+				{ "anchorPoint", new List<float> (){ 0.5f, 0.5f } },
+				{ "zOrders", new Dictionary<string, object> (){ { "local", 0 }, { "global", 0 } } }
+			};
+			return defaultAttrs.ContainsKey (attrName) ? defaultAttrs [attrName] : null;
+		}
+
+		public virtual void setAttr (string attrName, object val)
+		{
+
+		}
+
+		public virtual Dictionary<string, object> enumerateAttrs ()
+		{
+			Dictionary<string, object> ret = new Dictionary<string, object> ();
+			foreach (string attr in requiredAttrs) {
+				ret.Add (attr, getAttr (attr));
+			}
+			return ret;
+		}
+	}
+}
---
+++ sdk/js/sdk/IScreen.js
@@ -0,0 +1,12 @@
+var IScreen = function () {
+}
+
+IScreen.prototype.getPortSize = function () {
+    // return [width, height] in pixels of float type
+}
+
+IScreen.prototype.getScreen = function (width) {
+    // return promisable
+}
+        
+module.exports = IScreen;
