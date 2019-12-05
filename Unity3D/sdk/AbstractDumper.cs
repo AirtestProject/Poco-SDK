72016133d5e7464fceafc340ba7a63af18e89617
熊博 <gzxiongbo@corp.netease.com>
2019-12-05 11:36:57 +0800

    Add Unity3D readme, and reformat cs files

--- Unity3D/sdk/AbstractDumper.cs
+++ Unity3D/sdk/AbstractDumper.cs
@@ -6,59 +6,63 @@ using UnityEngine;
 
 namespace Poco
 {
-	public class AbstractDumper: IDumper
-	{
-		public virtual AbstractNode getRoot ()
-		{
-			return null;
-		}
+    public class AbstractDumper : IDumper
+    {
+        public virtual AbstractNode getRoot()
+        {
+            return null;
+        }
 
-		public Dictionary<string, object> dumpHierarchy ()
-		{
-			return dumpHierarchyImpl (getRoot (), true);
-		}
-		public Dictionary<string, object> dumpHierarchy (bool onlyVisibleNode)
-		{
-			return dumpHierarchyImpl (getRoot (), onlyVisibleNode);
-		}
+        public Dictionary<string, object> dumpHierarchy()
+        {
+            return dumpHierarchyImpl(getRoot(), true);
+        }
+        public Dictionary<string, object> dumpHierarchy(bool onlyVisibleNode)
+        {
+            return dumpHierarchyImpl(getRoot(), onlyVisibleNode);
+        }
 
-		private Dictionary<string, object> dumpHierarchyImpl (AbstractNode node, bool onlyVisibleNode)
-		{
-			if (node == null) {
-				return null;
-			}
+        private Dictionary<string, object> dumpHierarchyImpl(AbstractNode node, bool onlyVisibleNode)
+        {
+            if (node == null)
+            {
+                return null;
+            }
 
-			Dictionary<string, object> payload = node.enumerateAttrs ();
-			Dictionary<string, object> result = new Dictionary<string, object> ();
-			string name = (string)node.getAttr ("name");
-			result.Add ("name", name);
-			result.Add ("payload", payload);
+            Dictionary<string, object> payload = node.enumerateAttrs();
+            Dictionary<string, object> result = new Dictionary<string, object>();
+            string name = (string)node.getAttr("name");
+            result.Add("name", name);
+            result.Add("payload", payload);
 
-			List<object> children = new List<object> ();
-			foreach (AbstractNode child in node.getChildren()) {
-				if (!onlyVisibleNode || (bool)child.getAttr ("visible")) {
-					children.Add (dumpHierarchyImpl (child, onlyVisibleNode));
-				}
-			}
-			if (children.Count > 0) {
-				result.Add ("children", children);
-			}
-			return result;
-		}
+            List<object> children = new List<object>();
+            foreach (AbstractNode child in node.getChildren())
+            {
+                if (!onlyVisibleNode || (bool)child.getAttr("visible"))
+                {
+                    children.Add(dumpHierarchyImpl(child, onlyVisibleNode));
+                }
+            }
+            if (children.Count > 0)
+            {
+                result.Add("children", children);
+            }
+            return result;
+        }
 
-		public virtual List<float> getPortSize ()
-		{
-			return null;
-		}
-	}
+        public virtual List<float> getPortSize()
+        {
+            return null;
+        }
+    }
 
-	public interface IDumper
-	{
-		AbstractNode getRoot ();
+    public interface IDumper
+    {
+        AbstractNode getRoot();
 
-		Dictionary<string, object> dumpHierarchy ();
-		Dictionary<string, object> dumpHierarchy (bool onlyVisibleNode);
+        Dictionary<string, object> dumpHierarchy();
+        Dictionary<string, object> dumpHierarchy(bool onlyVisibleNode);
 
-		List<float> getPortSize ();
-	}
+        List<float> getPortSize();
+    }
 }
